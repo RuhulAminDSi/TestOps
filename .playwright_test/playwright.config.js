4b520c1d3030f8b63c3d6d24f9acf86114eddb51
Ruhul Amin <ruhulamin.dsi@gmail.com>
2026-04-12 23:53:41 +0600

    Configure Playwright to save test outputs in .playwright_test folder

--- .playwright_test/playwright.config.js
+++ .playwright_test/playwright.config.js
@@ -8,6 +8,9 @@ module.exports = defineConfig({
     headless: true,
     viewport: { width: 1280, height: 720 },
   },
+  outputDir: './.playwright_test/test-results',
+  screenshot: 'only-on-failure',
+  trace: 'retain-on-failure',
   projects: [
     {
       name: 'chromium',
